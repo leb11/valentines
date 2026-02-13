1c944e208f156b37968bbc020f5db94024c2c5ce
leb11 <lebjamesaspiras@gmail.com>
2026-02-13 11:47:43 +0800

    made mobile friendly

--- script.js
+++ script.js
@@ -1,42 +1,21 @@
-const text = "Will You Be My Valentine?";
-const typeTarget = document.getElementById("typeText");
-let index = 0;
-
-function typeEffect() {
-    if (index < text.length) {
-        typeTarget.innerHTML += text.charAt(index);
-        index++;
-        setTimeout(typeEffect, 80);
-    }
-}
-typeEffect();
-
-
 const noBtn = document.getElementById("noBtn");
-noBtn.addEventListener("mouseover", () => {
-    noBtn.style.left = Math.random() * 250 + "px";
-    noBtn.style.top = Math.random() * 150 + "px";
-});
-
-
 const yesBtn = document.getElementById("yesBtn");
 const message = document.getElementById("message");
+
+
+noBtn.addEventListener("touchstart", moveNo);
+noBtn.addEventListener("mouseover", moveNo);
+
+function moveNo() {
+    const maxX = 200;
+    const maxY = 60;
+
+    noBtn.style.left = Math.random() * maxX + "px";
+    noBtn.style.top = Math.random() * maxY + "px";
+}
+
 
 yesBtn.addEventListener("click", () => {
     message.style.display = "block";
     petals();
 });
-
-
-function petals() {
-    for (let i = 0; i < 30; i++) {
-        const petal = document.createElement("div");
-        petal.className = "petal";
-        petal.innerHTML = "🌸";
-        petal.style.left = Math.random() * 100 + "vw";
-        petal.style.animationDuration = (Math.random() * 3 + 4) + "s";
-        document.body.appendChild(petal);
-
-        setTimeout(() => petal.remove(), 6000);
-    }
-}

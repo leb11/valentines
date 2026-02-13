96c8c2ee879169233f96cdf304a684870a215611
leb11 <lebjamesaspiras@gmail.com>
2026-02-13 12:00:37 +0800

    minor changes

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
+const text = "Be mine forever? 💕";
+const typeTarget = document.getElementById("typeText");
+let index = 0;
+
+function typeEffect() {
+    if (index < text.length) {
+        typeTarget.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 80);
+    }
+}
+typeEffect();
+
+
 const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
 const message = document.getElementById("message");
 
-
-noBtn.addEventListener("touchstart", moveNo);
-noBtn.addEventListener("mouseover", moveNo);
-
 function moveNo() {
-    const maxX = 200;
-    const maxY = 60;
-
-    noBtn.style.left = Math.random() * maxX + "px";
-    noBtn.style.top = Math.random() * maxY + "px";
+    noBtn.style.left = Math.random() * 200 + "px";
+    noBtn.style.top = Math.random() * 60 + "px";
 }
 
+noBtn.addEventListener("mouseover", moveNo);
+noBtn.addEventListener("touchstart", moveNo);
 
 yesBtn.addEventListener("click", () => {
     message.style.display = "block";
     petals();
 });
+
+
+function petals() {
+    for (let i = 0; i < 30; i++) {
+        const petal = document.createElement("div");
+        petal.className = "petal";
+        petal.innerHTML = "🌸";
+        petal.style.left = Math.random() * 100 + "vw";
+        petal.style.animationDuration = (Math.random() * 3 + 4) + "s";
+        document.body.appendChild(petal);
+        setTimeout(() => petal.remove(), 6000);
+    }
+}
